7be6402add14778cdeb6c0fa7170bd72d8f0fabf
Corstiaan <corstiaanhesselink@gmail.com>
2015-05-28 17:19:14 +0200

    TTC

---
+++ Models/TagItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Fluxmatix.Mobile.Models
+{
+	public class TagItem
+	{
+		public string Key { get; private set; }
+		public string Text { get; private set; }
+
+		public TagItem (string key, string text)
+		{
+			Key = key;
+			Text = text;
+		}
+	}
+}
+
